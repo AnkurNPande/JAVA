5f91462adde79dd871382cefef3bbb653f8462f2
AnkurNPande <118685627+AnkurNPande@users.noreply.github.com>
2022-12-19 11:36:05 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Numbers
+{
+    public void EvenFactorsDisplay(int iNo)
+    {
+        int iCnt=0;
+
+        for (iCnt = 2; iCnt <=(iNo/2); iCnt+=2)
+        {
+          if((iNo % iCnt) == 0)
+            {
+                System.out.println("Even factor is : "+iCnt);
+            }
+        }
+    }
+}
+
+class Program2
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int iNo = sobj.nextInt();
+
+        Numbers nobj = new Numbers();
+
+        nobj.EvenFactorsDisplay(iNo);
+
+    }
+}
