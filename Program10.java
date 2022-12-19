dfc1ce75084eeb425d697442f26f2387ca51bd5e
AnkurNPande <118685627+AnkurNPande@users.noreply.github.com>
2022-12-19 22:30:33 +0530

    Add files via upload

---
+++ Program10.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+
+
+
+class MarvellousX
+{
+    public int SmallCount(String s)
+    {
+        int iCnt =0;
+       for (int i =0;i < s.length();i++)
+       {
+         if((s.charAt(i)>= 'a')&&(s.charAt(i)<='z'))
+         {
+             iCnt++;
+         }
+       }
+          return iCnt;
+      
+    }
+
+}
+
+class Program10
+
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Please  enter string");
+
+        String str = sobj.nextLine();
+
+        MarvellousX mobj = new MarvellousX();
+        int iRet = mobj.SmallCount(str);
+        System.out.println("Number of small letter is"+iRet);
+
+ 
+    }
+}
