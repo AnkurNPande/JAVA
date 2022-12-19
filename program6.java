cef79d162b9a171721f116bbff2bccf0030843c8
AnkurNPande <118685627+AnkurNPande@users.noreply.github.com>
2022-12-19 21:06:09 +0530

    Add files via upload

---
+++ program6.java
@@ -0,0 +1,103 @@
+ // Reverse the array.
+
+import java.util.*;
+
+class ArrayX
+{
+    protected int Arr[];
+
+
+    public ArrayX(int iSize)
+    {
+        Arr = new int [iSize];
+    }
+
+
+   protected void Accept()
+    {
+           Scanner sobj = new Scanner(System.in);
+
+           System.out.println("Please enter"+ Arr.length +"elements");
+           for (int iCnt = 0; iCnt < Arr.length;iCnt++)
+         {
+            System.out.println("Enter the element no :" +(iCnt+1) );
+
+            Arr[iCnt]= sobj.nextInt();
+         }
+          
+    }
+
+    protected void Display()
+    {
+        System.out.println("Elements of array are: ");
+
+        for(int iCnt = 0; iCnt < Arr.length;iCnt++)
+        {
+            System.out.print(Arr[iCnt]+"\t");
+        }
+        System.out.println();
+    }
+    
+}
+
+class MarvellousX extends ArrayX
+{
+    public MarvellousX(int iSize)
+    {
+        super(iSize);
+    }
+
+   public void Reverse()
+
+   {
+       int iStart =0;
+       int iEnd =Arr.length - 1;
+       int iTemp = 0;
+
+       while(iStart < iEnd)
+       {
+        iTemp = Arr[iStart];
+        Arr[iStart] = Arr[iEnd];
+        Arr[iEnd] = iTemp;
+
+        iStart++;
+        iEnd--;
+       }
+   }
+    
+
+}
+class Program6
+
+{
+    public static void main(String Arg[])
+
+
+    {
+
+        
+     Scanner sobj = new Scanner(System.in);
+
+     System.out.println("Enter the elements of array that you want to enter");
+
+        int iSize = sobj.nextInt();
+
+ MarvellousX obj = new MarvellousX(iSize);
+
+       obj.Accept();
+       obj.Display();
+
+       obj.Reverse();
+
+       System.out.println("Array after  reverse operation");
+
+       obj.Display();
+
+       obj.Reverse();
+
+       System.out.println("Array after  reverse operation");
+
+       obj.Display();
+        
+    }
+}
