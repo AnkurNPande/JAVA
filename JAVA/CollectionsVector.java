54ec08faedbbe01791b979dd4ad48be76a631c21
AnkurNPande <118685627+AnkurNPande@users.noreply.github.com>
2022-12-24 16:49:53 +0530

    Add files via upload

---
+++ JAVA/CollectionsVector.java
@@ -0,0 +1,28 @@
+  
+  ////////////////////////////////////////////////
+  // Vector is internally Synchronized in nature//
+  ////////////////////////////////////////////////
+  import java.util.*;
+
+class CollectionsVector
+{
+    public static void main(String arg[])
+    {
+        Vector <Float> aobj = new Vector<Float>();
+
+        aobj.add(10.6f);
+        aobj.add(20.6f);
+        aobj.add(30.6f);
+        aobj.add(40.6f);
+        aobj.add(50.6f);
+
+        Iterator iobj = lobj.iterator();
+
+        while(iobj.hasNext())
+        {
+            System.out.println(iobj.next());
+        }
+
+        aobj.clear();
+    }
+}
